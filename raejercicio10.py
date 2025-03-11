18b820112d7079ac19bea9c485c77dce68f19d06
AnaRaquelcv <anaraquel2004cv@gmail.com>
2025-03-11 09:15:54 -0400

    Add files via upload

---
+++ raejercicio10.py
@@ -0,0 +1,5 @@
+num1=int(input("Ingrese un numero para la suma;"))
+num2=int(input("Ingrese un numero para la suma;"))
+
+sum= num1+num2
+print(f"El resultado de la suma es de {sum}")
---
+++ raejercicio9.py
@@ -0,0 +1,5 @@
+nombre=input("Ingrese su nombre;")
+edad=int(input("Ingrese su edad;"))
+pais=input("ingrese su pais de origen;")
+
+print(f"la persona {nombre} tiene la edad de {edad} años y vive en el pais {pais}")
