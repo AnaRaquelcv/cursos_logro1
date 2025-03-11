a651f2581c9fdea16d39cd33510f1d0d29dbf1d5
AnaRaquelcv <anaraquel2004cv@gmail.com>
2025-03-11 11:42:18 -0400

    Add files via upload

---
+++ raejercicio14.py
@@ -0,0 +1,11 @@
+nota = int(input("Ingrese su nota: "))
+if nota >=90:
+    print("su nota es A")
+elif nota >=80:
+    print("su nota es B")
+elif nota >=70:
+    print("su nota es C")
+elif nota >=60:
+    print("su nota es D")
+else:
+    print("su nota es F")
